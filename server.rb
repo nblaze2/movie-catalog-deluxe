0016834024368f47d2a6ebeceee9f96eb94be9ac
Nick Blaisdell <nick.blaisdell@gmail.com>
2016-11-11 14:52:52 -0500

    first

---
+++ server.rb
@@ -0,0 +1,21 @@
+require "sinatra"
+require "pg"
+
+set :bind, '0.0.0.0'  # bind to all interfaces
+
+configure :development do
+  set :db_config, { dbname: "movies" }
+end
+
+configure :test do
+  set :db_config, { dbname: "movies_test" }
+end
+
+def db_connection
+  begin
+    connection = PG.connect(Sinatra::Application.db_config)
+    yield(connection)
+  ensure
+    connection.close
+  end
+end
