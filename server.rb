14f4f3cd2b73f417231a4ad04e5ee48741dd0c17
Nick Blaisdell <nick.blaisdell@gmail.com>
2016-11-11 15:21:07 -0500

    added css (might not use) basic pages up

--- server.rb
+++ server.rb
@@ -19,3 +19,31 @@ def db_connection
     connection.close
   end
 end
+
+get "/actors" do
+  # @teams = ["Simpson Slammers", "Jetson Jets", "Flintstone Fire", "Griffin Goats"]
+  erb :'actors/index'
+end
+
+get "/actors/:id" do
+  # @team_name = params[:team_name]
+  # @team_data = TeamData::ROLL_CALL[@team_name.to_sym]
+  erb :'actors/show'
+end
+
+get "/movies" do
+  # @positions = TeamData::ROLL_CALL.values[0].keys
+  erb :'movies/index'
+end
+
+get "/movies/:id" do
+  # @position = params[:position]
+  # @whole_league = TeamData::ROLL_CALL
+  # @players_by_position = {}
+  #
+  # @whole_league.each_pair do |team, roster|
+  #   @players_by_position[roster[@position.to_sym]] = team
+  # end
+  #
+  erb :'movies/show'
+end
